b20426591f2c19c693f0f8537474aebc6d59b5ba
omoinjm <njmcloud@gmail.com>
2023-02-03 15:00:48 +0200

    all things looking sexy

--- graphql/index.ts
+++ graphql/index.ts
@@ -14,7 +14,7 @@ const httpTrigger: AzureFunction = async function (
 	});
 
 	context.res = {
-		status: 200 /* Defaults to 200 */,
+		status: 200,
 		headers: {
 			'Content-Type': 'application/json',
 		},
